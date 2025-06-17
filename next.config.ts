e6af7be5d07158486683f3d802999ca20b835a7b
fardin42 <fssaiyyad42@gmail.com>
2025-06-17 21:13:38 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/antarang',
+  assetPrefix: '/antarang',
   images: {
     remotePatterns: [
       {
